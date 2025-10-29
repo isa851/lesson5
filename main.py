e957e977e42fab992b95b97f4d26b18ec4acfae2
isa851 <isa904363@gmail.com>
2025-10-29 19:47:09 +0600

    lesson5

---
+++ main.py
@@ -0,0 +1,16 @@
+# встроенные ммодули
+import random
+import os
+import math
+import datetime
+import sys
+
+# from my_module import hello
+#
+# print(hello("Islam"))
+#
+#
+# import requests
+#
+# response = requests.get("https://api.github.com")
+# print(response.status_code)
